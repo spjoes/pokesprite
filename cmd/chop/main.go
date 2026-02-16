3335b453f4fc2d67d0ab14c5583d71c4f4245b0d
Joey <joeykerp@gmail.com>
2026-02-15 17:52:45 -0800

    add scss based chop

--- cmd/chop/main.go
+++ cmd/chop/main.go
@@ -1,11 +1,14 @@
 // This script is used to take an existing spritesheet and chop it up into
-// individual sprites. It reads from the filename passed as the first argument
-// to get all the necessary information about this spritesheet. This is a JSON
-// file that we created, and it's structure is defined by the `Data` type below.
-// This was first created to handle the combine spritesheet for Legends: Arceus
-// found at https://www.spriters-resource.com/fullview/168439/ (regular) and
-// https://www.spriters-resource.com/fullview/168440/ (shiny), but it shouldn't
-// be exclusive to those.
+// individual sprites. It supports two modes:
+//
+// 1) JSON mode: Pass a .json file that describes the spritesheet (filename,
+//    columns, rows, outline, padding, and a list of Pokemon). Used for
+//    sheets like Legends: Arceus from spriters-resource.
+//
+// 2) SCSS mode: Pass a .scss file (e.g. toExtractFrom/pokesprite.scss). The
+//    script finds pokesprite.png in the same directory and parses the SCSS
+//    for width, height, and background-position of each .pkicon rule, then
+//    extracts each sprite into ./images/ with the correct filenames.
 package main
 
 import (
@@ -17,6 +20,9 @@ import (
 	_ "image/png"
 	"io/ioutil"
 	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
 )
 
 type Data struct {
@@ -38,13 +44,137 @@ type Pokemon struct {
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Println("Usage: task chop -- <filename.json>")
-		fmt.Println("Example: task chop -- ./data/spritesheet.json")
+		fmt.Println("Usage: task chop -- <filename.json|filename.scss>")
+		fmt.Println("  JSON: task chop -- ./data/spritesheet.json")
+		fmt.Println("  SCSS: task chop -- toExtractFrom/pokesprite.scss")
 		os.Exit(1)
 	}
 
 	filename := os.Args[1]
 
+	if strings.HasSuffix(filename, ".scss") {
+		chopFromSCSS(filename)
+		return
+	}
+
+	chopFromJSON(filename)
+}
+
+// chopFromSCSS reads a pokesprite.scss and pokesprite.png from the same
+// directory and extracts each sprite into ./images/.
+func chopFromSCSS(scssPath string) {
+	dir := filepath.Dir(scssPath)
+	pngPath := filepath.Join(dir, "pokesprite.png")
+
+	raw, err := ioutil.ReadFile(scssPath)
+	if err != nil {
+		panic(err)
+	}
+	imgFile, err := os.Open(pngPath)
+	if err != nil {
+		panic(fmt.Errorf("open spritesheet %s: %w", pngPath, err))
+	}
+	defer imgFile.Close()
+	img, _, err := image.Decode(imgFile)
+	if err != nil {
+		panic(err)
+	}
+
+	// Match lines like: .pkicon.pkicon-001.color-shiny { width: 20px; height: 19px; background-position: 0px -56px; }
+	// or .pkicon.pkicon-ball-love { width: 18px; height: 18px; background-position: 0px 0px; }
+	lineRE := regexp.MustCompile(`\.pkicon\.pkicon-([^\s{]+)\s*\{\s*width:\s*(\d+)px;\s*height:\s*(\d+)px;\s*background-position:\s*(-?\d+)px\s*(-?\d+)px;`)
+	lines := strings.Split(string(raw), "\n")
+
+	for _, line := range lines {
+		matches := lineRE.FindStringSubmatch(line)
+		if matches == nil {
+			continue
+		}
+		selectorParts := matches[1]
+		w := atoi(matches[2])
+		h := atoi(matches[3])
+		bgX := atoi(matches[4])
+		bgY := atoi(matches[5])
+
+		outName := scssSelectorToFilename(selectorParts)
+		if outName == "" {
+			continue
+		}
+
+		// CSS background-position is the offset of the sprite (negative in the sheet).
+		srcX := -bgX
+		srcY := -bgY
+		if srcX < 0 || srcY < 0 || srcX+w > img.Bounds().Dx() || srcY+h > img.Bounds().Dy() {
+			fmt.Fprintf(os.Stderr, "chop: skip %s (bounds %d,%d %dx%d outside image)\n", outName, srcX, srcY, w, h)
+			continue
+		}
+
+		r := image.Rect(0, 0, w, h)
+		rgba := image.NewRGBA(r)
+		draw.Draw(rgba, r, img, image.Point{srcX, srcY}, draw.Src)
+
+		outPath := filepath.Join("images", outName)
+		out, err := os.Create(outPath)
+		if err != nil {
+			panic(err)
+		}
+		if err := png.Encode(out, rgba); err != nil {
+			out.Close()
+			panic(err)
+		}
+		out.Close()
+	}
+}
+
+func atoi(s string) int {
+	var n int
+	fmt.Sscanf(s, "%d", &n)
+	return n
+}
+
+// scssSelectorToFilename converts the middle part of a .pkicon.pkicon-XXX... selector
+// to a filename matching the repo convention: 001.png, 001-shiny.png, 025-legends_arceus.png, love-ball.png, etc.
+func scssSelectorToFilename(parts string) string {
+	segments := strings.Split(parts, ".")
+	var id string
+	var form, gameFamily string
+	var shiny bool
+	for _, s := range segments {
+		switch {
+		case s == "color-shiny":
+			shiny = true
+		case strings.HasPrefix(s, "form-"):
+			form = strings.TrimPrefix(s, "form-")
+		case strings.HasPrefix(s, "game-family-"):
+			gameFamily = strings.TrimPrefix(s, "game-family-")
+		case strings.HasPrefix(s, "pkicon-"):
+			// shouldn't appear in this segment string
+		default:
+			// id: "001" or "ball-love" or "025"
+			id = s
+		}
+	}
+	if id == "" {
+		return ""
+	}
+	if id == "ball-love" {
+		return "love-ball.png"
+	}
+	name := id
+	if shiny {
+		name += "-shiny"
+	}
+	if gameFamily != "" {
+		name += "-" + gameFamily
+	}
+	if form != "" {
+		name += "-" + form
+	}
+	return name + ".png"
+}
+
+
+func chopFromJSON(filename string) {
 	raw, err := ioutil.ReadFile(filename)
 	if err != nil {
 		panic(err)
@@ -90,17 +220,17 @@ func main() {
 		draw.Draw(rgba, r.Bounds(), img, image.Point{column*height + (column+1)*data.Outline + data.Padding, row*width + (row+1)*data.Outline + data.Padding}, draw.Src)
 
 		// Generate the new filename.
-		filename := fmt.Sprintf("./images/%03d", pokemon.ID)
+		outName := fmt.Sprintf("./images/%03d", pokemon.ID)
 		if data.Suffix != nil {
-			filename += "-" + *data.Suffix
+			outName += "-" + *data.Suffix
 		}
 		if pokemon.Form != nil {
-			filename += "-" + *pokemon.Form
+			outName += "-" + *pokemon.Form
 		}
-		filename += ".png"
+		outName += ".png"
 
 		// Write the new chopped up png out.
-		out, err := os.Create(filename)
+		out, err := os.Create(outName)
 		if err != nil {
 			panic(err)
 		}
